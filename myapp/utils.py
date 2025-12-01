bffb3f4a51f2499293241acc1571a297ce5dc217
Rajjivani27 <rajjivani027@gmail.com>
2025-12-01 20:26:10 +0530

    Added functionality for adding ai_summary,severity and suggestion to fix bug for every bug encountered

---
+++ myapp/utils.py
@@ -0,0 +1,46 @@
+import re
+import google.generativeai as genai
+from ai_bug_reporter import settings
+from .models import *
+
+genai.configure(api_key=settings.GOOGLE_API_KEY)
+
+model = genai.GenerativeModel('gemini-2.5-flash')
+
+chat_session = model.start_chat(history=[])
+
+def get_ai_summary(bug : BugReporter):
+    error_message = bug.error_message
+    stack_trace = bug.stack_trace
+    pageUrl = bug.page_url
+    screenshot = bug.screenshot
+    userAgent = bug.user_agent
+
+    question = f"""error_message:{error_message}\n
+                    stack_tarce:{stack_trace}\n
+                    page_url:{pageUrl}\n
+                    screenshot:{screenshot}\n
+                    user_agent:{userAgent}\n  
+                    Here are the details of a javascript runtime error , 
+                    give me answer in three separate lines and put \n at ending of every line, 
+                    first line should contain summary with heading Summary, 
+                    second line should contain severity of error from 1 to 5, 1 means low and 5 means high, only return a number as answer with Severity heading 
+                    and third line should contain suggestion to fix it with heading Suggestion and all suggestions should be continue"""
+    
+    response = chat_session.send_message(question)
+
+    lines = response.text.split("\n")
+
+    for line in lines:
+        if line.startswith('Suggestion'):
+            suggestion = line[12:]
+            bug.suggestion = suggestion
+        elif line.startswith('Severity'):
+            severity = line[10:]
+            bug.severity = severity
+        elif line.startswith('Summary'):
+            bug.ai_summary = line
+
+    bug.save()
+
+    return bug
--- myapp/views.py
+++ myapp/views.py
@@ -4,10 +4,21 @@ from .serializers import *
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework import status
+from .utils import get_ai_summary
 
 class BugReporterViewSet(ModelViewSet):
     def get_queryset(self):
         return BugReporter.objects.all()
+    
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        bug = serializer.save()
+
+        get_ai_summary(bug)
+
+        return Response(serializer.data,status=status.HTTP_201_CREATED)
     
     def get_serializer(self, *args, **kwargs):
         return BugReporterSerializer(*args,context=self.get_serializer_context(),**kwargs)
